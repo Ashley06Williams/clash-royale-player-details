52e7f6ecfc924b12b6ce816d1bb2d8834d6c464d
Ashley06Williams <ashley@waterfalldigital.co.za>
2024-10-19 20:06:55 +0200

    Implemented/basic-design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import MainDisplay from "@/components/MainDIsplay";
+import PlayerDetails from "@/components/PlayerDetails";
+import SearchBar from "@/components/SearchBar";
+
 export default function Home() {
-  return;
+  return (
+    <div className="flex h-screen w-screen bg-gradient-to-r from-red to-blue-clash items-center justify-center">
+      <div className="bg-blue h-[800px] w-1/2 rounded-3xl shadow-2xl p-2">
+        <SearchBar />
+        <MainDisplay />
+        <PlayerDetails />
+      </div>
+    </div>
+  );
 }
